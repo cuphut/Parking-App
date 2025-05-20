fe859be9bca4032568b0a9501c40ad488b8f23f2
Cu Phu <cuphut@gmail.com>
2025-05-20 17:18:47 +0700

    update

--- frontend/app/lib/services/parking_service.dart
+++ frontend/app/lib/services/parking_service.dart
@@ -3,11 +3,9 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class ParkingService {
-  static const String _baseUrl = 'http://192.168.100.145:8001';
-
+  final baseUrl = dotenv.env['BASE_URL'] ?? 'http://default-url.com';
   // Lấy danh sách bãi đỗ
   Future<List<dynamic>> getParking() async {
-    final baseUrl = dotenv.env['BASE_URL'] ?? 'http://default-url.com';
     try {
       final response = await http.get(
         Uri.parse('$baseUrl/parking-lot/no-exit-time'),
@@ -26,7 +24,7 @@ class ParkingService {
   Future<List<dynamic>> getAllParking() async {
     try {
       final response = await http.get(
-        Uri.parse('$_baseUrl/parking-lot/'),
+        Uri.parse('$baseUrl/parking-lot/'),
       );
       if (response.statusCode == 200) {
         final jsonData = json.decode(response.body);
